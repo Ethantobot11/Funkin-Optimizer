dc6b640f20c4d62c0acfaf6b1a80c6151426299c
Ethantobot <ethantobot7@gmail.com>
2026-03-27 16:02:17 +0100

    Update Variables.hx

--- source/Variables.hx
+++ source/Variables.hx
@@ -2,7 +2,7 @@ package;
 
 class Variables {
     public static var shinksprsh = false;
-    public static var minlua = false;
+    public static var minhx = false;
     public static var minxml = false;
     public static var jpegcomp = false;
     public static var minchart = false;
